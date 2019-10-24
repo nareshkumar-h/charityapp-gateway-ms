a023256eacd6c97718fed46a2fa5ba440f91f533
unknown <yaswanthlakshmi143@gmail.com>
2019-10-24 23:04:52 +0530

    add CharityappGatewayMs Application

--- src/main/java/com/revature/charityappgatewayms/CharityappGatewayMsApplication.java
+++ src/main/java/com/revature/charityappgatewayms/CharityappGatewayMsApplication.java
@@ -2,8 +2,10 @@ package com.revature.charityappgatewayms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CharityappGatewayMsApplication {
 
 	public static void main(String[] args) {
